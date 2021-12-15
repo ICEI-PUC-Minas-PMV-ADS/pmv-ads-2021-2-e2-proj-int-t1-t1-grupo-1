5eda573f3355d821076696fd6e3829acb122ec86
Rodrigo Lobenwein <62033829+rlobenwein@users.noreply.github.com>
2021-12-15 18:45:30 -0300

    update

--- src/MinhaSaude/MinhaSaude/Controllers/HomeController.cs
+++ src/MinhaSaude/MinhaSaude/Controllers/HomeController.cs
@@ -1,13 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MinhaSaude.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MinhaSaude.Controllers
 {
@@ -22,29 +16,8 @@ namespace MinhaSaude.Controllers
 
         public IActionResult Index()
         {
-            ////Fetch all files in the Folder (Directory).
-            //string[] filePaths = Directory.GetFiles(Path.Combine(this.Environment.WebRootPath, "receitas/"));
-
-            ////Copy File names to Model collection.
-            //List<FileModel> files = new List<FileModel>();
-            //foreach (string filePath in filePaths)
-            //{
-            //    files.Add(new FileModel { FileName = Path.GetFileName(filePath) });
-            //}
-
             return View();
         }
-        //public FileResult DownloadFile(string fileName)
-        //{
-        //    //Build the File Path.
-        //    string path = Path.Combine(this.Environment.WebRootPath, "receitas/") + fileName;
-
-        //    //Read the File data into Byte Array.
-        //    byte[] bytes = System.IO.File.ReadAllBytes(path);
-
-        //    //Send the File to Download.
-        //    return File(bytes, "application/octet-stream", fileName);
-        //}
 
         public IActionResult Privacy()
         {
